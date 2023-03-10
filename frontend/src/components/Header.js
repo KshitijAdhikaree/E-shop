73a8b90cad2effdee56d90b62f1b990a19a62d74
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-10 22:02:26 +0545

    nav bar start

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,33 @@
-import React from 'react'
+// import React from 'react'
 
+// import { Nav, Container, Navbar } from 'react-bootstrap'
+
+// const Header = () => {
+//   return (
+//     <header>
+//       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+//         <Container>
+//           <Navbar.Brand href='/'>E-Shop</Navbar.Brand>
+//           <Navbar.Toggle aria-controls='basic-navbar-nav' />
+//           <Navbar.Collapse id='basic-navbar-nav'>
+//             <Nav className='ml-auto'>
+//               <Nav.Link href='/cart'>
+//                 <i className='fas fa-shopping-cart'></i> Cart
+//               </Nav.Link>
+//               <Nav.Link href='/login'>
+//                 <i className='fas fa-user'></i>Sign In
+//               </Nav.Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     </header>
+//   )
+// }
+
+// export default Header
+
+import React from 'react'
 import { Nav, Container, Navbar } from 'react-bootstrap'
 
 const Header = () => {
@@ -10,7 +38,7 @@ const Header = () => {
           <Navbar.Brand href='/'>E-Shop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='justify-content-end'>
+            <Nav className='ml-auto'>
               <Nav.Link href='/cart'>
                 <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
@@ -26,3 +54,4 @@ const Header = () => {
 }
 
 export default Header
+
