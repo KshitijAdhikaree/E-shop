783c38936572a2e90acb20c89ef6e7d23206605e
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-11 15:17:27 +0545

    rating component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
-  return <footer>
-    <Container>
+  return (
+    <footer>
+      <Container>
         <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; E-shop
             <Col className='text-center py-3'>
-                Copyright &copy; E-shop
+              Designed and Developed by:
+              <Col className='text-center py-1'>
+                <a href='https://kshitijadhikaree.com.np/'>Kshitij Adhikaree</a>
+              </Col>
             </Col>
+          </Col>
         </Row>
-    </Container>
-  </footer>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
