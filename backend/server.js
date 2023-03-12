b38b2f767d2f1ff943d1d2a84ff5f9cd27ab87ef
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-12 14:03:32 +0545

    connect to db and add colors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
+import colors from 'colors'
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -22,5 +27,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
