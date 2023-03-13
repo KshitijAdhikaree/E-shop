0a2f36aaad20535ebbb6bc0e7012d8d75b77fade
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-13 17:26:44 +0545

    bringing redux state to homescreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,21 +5,23 @@ import {
   PRODUCT_LIST_FAIL,
 } from '../constants/productConstants'
 
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST })
 
-export const listProducts = () => async(dispatch) => {
-try {
-    dispatch({ type: PRODUCT_LIST_REQUEST})
-
-    const {data} = await axios.get('/api/products')
+    const { data } = await axios.get('/api/products')
 
     dispatch({
-        type: PRODUCT_LIST_SUCCESS,
-        payload : data
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
     })
-} catch (error) {
+  } catch (error) {
     dispatch({
-        type: PRODUCT_LIST_FAIL,
-        payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
     })
-}
+  }
 }
