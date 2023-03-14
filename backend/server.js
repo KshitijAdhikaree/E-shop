6caecdc9dec011edcf75b13f8b6b8f52e7b172c2
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-14 20:20:08 +0545

    user authentication and endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import colors from 'colors'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -13,22 +14,24 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is runnig')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(
-    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
   )
 )
